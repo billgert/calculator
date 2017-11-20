0f6ebaf93589a1127f2d244f45146a59a5674012
Patrik Billgert <patrikbillgert@Patriks-MacBook-Pro.local>
2017-11-19 19:01:56 -0500

    improved code and keeping it alive until user tells not to

--- main.c
+++ main.c
@@ -2,5 +2,27 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello World\n");
+  int num1, num2;
+  char operator;
+  char quit;
+
+  do {
+    printf("Operation: ");
+    scanf( "%i %c %i", &num1, &operator, &num2);
+
+    if (operator == '+') {
+      printf("Result: %i\n", num1 + num2);
+    } else if (operator == '-') {
+      printf("Result: %i\n", num1 - num2);
+    } else if (operator == '/') {
+      printf("Result: %i\n", num1 / num2);
+    } else if (operator == '*') {
+      printf("Result: %i\n", num1 * num2);
+    } else {
+      printf("operator not supported\n");
+    }
+
+    printf("Exit: ");
+    scanf(" %c", &quit);
+  } while(quit == 'N' || quit == 'n');
 }
