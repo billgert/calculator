ce53cdf4caf64ab7868388b44e74e5e836d4e841
Patrik Billgert <patrikbillgert@Patriks-MacBook-Pro.local>
2017-11-20 21:25:26 -0500

    separated functionality

--- main.c
+++ main.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int num1, num2;
+char operator;
+char answer;
+
+void input();
+void listen();
+void question();
+
 int main() {
-  int num1, num2;
-  char operator;
-  char quit;
-
   do {
-    printf("Operation: ");
-    scanf( "%i %c %i", &num1, &operator, &num2);
+    input();
+    listen();
+    question();
+  } while(answer == 'N' || answer == 'n');
+}
 
-    if (operator == '+') {
-      printf("Result: %i\n", num1 + num2);
-    } else if (operator == '-') {
-      printf("Result: %i\n", num1 - num2);
-    } else if (operator == '/') {
-      printf("Result: %i\n", num1 / num2);
-    } else if (operator == '*') {
-      printf("Result: %i\n", num1 * num2);
-    } else {
-      printf("operator not supported\n");
-    }
+void input() {
+  printf("Operation: ");
+  scanf( "%i %c %i", &num1, &operator, &num2);
+}
 
-    printf("Exit: ");
-    scanf(" %c", &quit);
-  } while(quit == 'N' || quit == 'n');
+void listen() {
+  if (operator == '+') {
+    printf("Result: %i\n", num1 + num2);
+  } else if (operator == '-') {
+    printf("Result: %i\n", num1 - num2);
+  } else if (operator == '/') {
+    printf("Result: %i\n", num1 / num2);
+  } else if (operator == '*') {
+    printf("Result: %i\n", num1 * num2);
+  } else {
+    printf("Operator not supported\n");
+  }
+}
+
+void question() {
+  printf("Exit: ");
+  scanf(" %c", &answer);
 }
